5c02d02c59f010e5d2eaaad6eaa19b809782263a
chirayu-joshi <chirayujoshi1189@gmail.com>
2019-08-04 19:50:57 +0530

    timer added

---
+++ js/timer.js
@@ -0,0 +1,14 @@
+var deadline = new Date("sep 13, 2019 00:00:00").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var timeLeft = deadline - now;
+    var days = Math.floor(timeLeft / (1000*60*60*24));
+    var hours = Math.floor((timeLeft % (1000*60*60*24)) / (1000*60*60));
+    var minutes = Math.floor((timeLeft % (1000*60*60)) / (1000*60));
+    var seconds = Math.floor((timeLeft % (1000*60)) / 1000);
+    document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    if(t < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+}, 1000);
