341963c02041b7381acd8ba036b15eb03ff9669c
chirayu-joshi <chirayujoshi1189@gmail.com>
2019-08-04 14:47:10 +0530

    meta-tags added

--- comingsoon.php
+++ comingsoon.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Cognizance 2019</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cognizance 2019</title>
 </head>
 <body>
-<h1>Coming Soon!!</h1>
+    <h1>Coming Soon!!</h1>
 </body>
 </html>
