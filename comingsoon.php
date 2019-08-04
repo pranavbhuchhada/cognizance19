17db20100c9765eb639b59d0581275ba7df4d9f9
pranavbhuchhada <pranavbhuchhada@users.noreply.github.com>
2019-08-04 18:26:12 +0530

    Contactus completed

--- comingsoon.php
+++ comingsoon.php
@@ -8,25 +8,30 @@
 	<link rel="stylesheet" type="text/css" href="css/comingsoon.css?v=1">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="shortcut icon" type="image/x-icon" href="assets/cz_logo.ico" />
 </head>
 <body>
-	<div class="container-fluid">
-		<canvas id = "canvas" style="z-index: -100"></canvas>
+		<div class="container-fluid">
 		<div class="row">
-			<div id="logo" class="text-center col-4">
+			<div id="logo" class="text-center col-lg-4 col-md-4 col-sm-12 col-xs-12">
 				<img id="cz_logo" src="assets/cz_logo.png" class="img-fluid" draggable="false">
-				<br><button id="contact" class="btn btn-info" onclick="window.location.href='#contactus'">Contact Us!!</button>
+				<br><button id="contact" class="btn btn-info" onclick="window.location.href='#contactus';">Contact Us!!</button>
+				<br><br>
+				<h1 style="font-family: MontserratLightItalic;color: #ff9615">COMING SOON!!</h1>
+				<h4 style="font-family: MontserratLightItalic;color: #33cc66;">WORKING ON AWESOME WEBSITE!!</h4>
 			</div>
 		</div>
 	</div>
-
+	<canvas id = "canvas" style="z-index: -100"></canvas>
 	<footer id="contactus">
 	<div class="container">
+		<div></div>
 		<div class="row justify-content-center">
 			<div class="col-12 text-center pt-5">
+				<a href="mailto:charusat.cognizance19@gamil.com?Subject=Cognizance%2019" class="fa fa-envelope-o" title="charusat.cognizance19@gamil.com"></a>
 				<a href="https://www.facebook.com/czCHARUSAT/" target="_blank" class="fa fa-facebook"></a>
 				<a href="https://www.instagram.com/cognizance_charusat/" target="_blank" class="fa fa-instagram"></a>
-				<p class="m-3">Copyright © <?php echo date("Y"); ?>. All rights reserved</p>
+				<p style="color: white;" class="m-3">Copyright © <?php echo date("Y"); ?>. All rights reserved</p>
 			</div>
 		</div>
 	</div>
