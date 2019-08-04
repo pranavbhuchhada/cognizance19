e70ec06dc33407d8682fc91bc94444765091236b
pranavbhuchhada <pranavbhuchhada@users.noreply.github.com>
2019-08-04 16:51:10 +0530

    Logo and Background Added

--- comingsoon.php
+++ comingsoon.php
@@ -2,8 +2,18 @@
 <html>
 <head>
 	<title>Cognizance 2019</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="plugins/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/comingsoon.css?v=1">
 </head>
 <body>
-<h1>Coming Soon!!</h1>
+	<canvas id = "canvas" style="z-index: -100"></canvas>
+	<div id="logo" class="text-center">
+		<img id="cz_logo" src="assets/cz_logo.png" class="img-fluid" draggable="false">
+	</div>
+<script type="text/javascript" src="plugins/jQuery/jquery.min.js"></script>
+<script type="text/javascript" src="plugins/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/comingsoon.js"></script>
 </body>
 </html>
