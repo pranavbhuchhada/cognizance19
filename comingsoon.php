1abcca1e58fe29984056801e4f60c40755315d0b
pranavbhuchhada <pranavbhuchhada@users.noreply.github.com>
2019-08-05 20:17:55 +0530

    CZ Dates Changed

--- comingsoon.php
+++ comingsoon.php
@@ -4,41 +4,32 @@
 	<title>Cognizance 2019</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="plugins/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/comingsoon.css?v=1">
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons|Teko:400,700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="shortcut icon" type="image/x-icon" href="assets/cz_logo.ico" />
 </head>
 <body>
 	<div class="container-fluid" style="height: 100%">
-		<div id="main" class="d-flex flex-wrap align-content-center justify-content-center" style="margin-bottom: 100px;">
-		<div class="row d-flex d-flex justify-content-center" style="height: 100%;">
-			<div id="logo" class="text-center col-lg-4 col-md-4 col-sm-12 col-xs-12 align-self-center d-flex flex-wrap align-content-center justify-content-center">
-				<img id="cz_logo" src="assets/cz_logo.png" class="img-fluid" draggable="false">
-				<h1 style="font-family: MontserratBold;color: #ff9615;margin-top: 30px;">12-13 SEPTEMBER 2019</h1>
-				<h1 style="font-family: MontserratBold;color: #33cc66;padding-top: 20px;">Stay Tuned !!</h1>
-				<br>
-				<p id="timer"></p>
+		<div id="main" style="margin-bottom: 100px;">
+			<div class="row  justify-content-center mt-5" style="height: 100%;">
+				<div id="logo" class="text-center col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<img id="cz_logo" src="assets/cz_logo.png" class="img-fluid" draggable="false">
+					<h1 style="font-family: MontserratBold;color: #ff9615;margin-top: 30px;">13-14 SEPTEMBER 2019</h1>
+					<h1 style="font-family: MontserratBold;color: #33cc66;padding-top: 20px;">Stay Tuned !!</h1>
+					<br>
+					<h1 id="timer"></h1>
+				</div>
 			</div>
-		</div>
 		</div>
 		<canvas id="canvas"></canvas>
 	</div>
 	<footer id="contactus">
 	<div class="container-fluid" >
 		<div class="row justify-content-center">
-			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 text-center pt-5">
-				<img  class="img-fluid" src="assets/ecc_logo.png" style="width: 100px;">
-			</div>
-			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 pt-5 text-center">
-				<a href="mailto:info@cz19.in?Subject=Inquery%20CZ'19" class="fa fa-envelope-o" title="mailto:info@cz19.in"></a>
-				<a href="https://www.facebook.com/czCHARUSAT/" target="_blank" class="fa fa-facebook"></a>
-				<a href="https://www.instagram.com/cognizance_charusat/" target="_blank" class="fa fa-instagram"></a>
-			</div>
 			<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12 text-center pt-5">
 				<a href="http://charusat.edu.in/" target="_blank">
-					<img class="img-fluid" src="assets/charusat_logo.png">
+					<img class="img-fluid" src="assets/charusat_logo.jfif">
 				</a>
 			</div>
 			<div class="col-lg-1 col-md-1 col-sm-12 col-xs-12 text-center pt-5">
@@ -49,12 +40,20 @@
 				<a href="https://www.charusat.ac.in/DPIATR" target="_blank">
 					<img class="img-fluid" src="assets/depstar_logo.jpg" style="width: 100px;"></a>
 			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 pt-5 text-center">
+				<a href="mailto:info@cz19.in?Subject=Inquery%20CZ'19" class="fa fa-envelope-o" title="mailto:info@cz19.in"></a>
+				<a href="https://www.facebook.com/czCHARUSAT/" target="_blank" class="fa fa-facebook"></a>
+				<a href="https://www.instagram.com/cognizance_charusat/" target="_blank" class="fa fa-instagram"></a>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 text-center pt-5">
+				<img  class="img-fluid" src="assets/ecc_logo.png" style="width: 100px;">
+			</div>
 			<p style="color: white;" class="m-3">Copyright © <?php echo date("Y"); ?>. All rights reserved</p>
 		</div>
 	</div>
 	</footer>
-	<script type="text/javascript" src="plugins/jQuery/jquery.min.js"></script>
-	<script type="text/javascript" src="plugins/bootstrap/js/bootstrap.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/comingsoon.js"></script>
 	<script type="text/javascript" src="js/timer.js"></script>
 </body>
