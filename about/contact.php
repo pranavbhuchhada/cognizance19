2ef36dbf9380a785c12aa22b80fd888635d311f1
mitraj30 <39413021+mitraj30@users.noreply.github.com>
2019-08-11 18:18:51 +0530

    Add files via upload

---
+++ about/contact.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+ <link rel="stylesheet" type="text/css" href="cc2.css">
+	 <link rel="stylesheet" type="text/css" href="fonts/stylesheet.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<title>Contact Us</title>
+</head>
+
+<body class="bd">
+	<script
+  src="https://code.jquery.com/jquery-2.0.0.js"
+  integrity="sha256-iW43nTNM8LFseNmWKhV5FHFW1KcjVQMvzg3l9nPU4oc="
+  crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	
+	
+  <div class="container d-flex justify-content-center">
+	  <div class="row">
+	  <div class="col-md-12 col-sm-12 col-xs-12" data-animate-scroll='{"y":"50", "duration": "1.75","scaleX": "0","scaleY": "0"}'>
+		  <div class="hd">
+		  <p class="hdd">CONTACT US</p>
+		  </div>
+		  
+		  </div>
+	  </div>
+	
+	</div>
+		 <div class="container">
+	<div class="row">
+			
+			 <div class="col-md-6 col-sm-6 col-xs-6 so1 img-fluid" data-animate-scroll='{"x": "-200","y": "0", "alpha": "0", "duration": "1.75"}'>
+		<div class="so">
+		 
+				 <div class="ex">
+					 
+					 <h1 class="head">CHAROTAR UNIVERSITY OF SCIENCE AND TECHNOLOGY </h1>
+					   
+					<p class="ad1" >CHARUSAT Campus,Highway 139,</p>
+						 
+					  <p class="ad2">Off Nadiad - Petlad Road, Changa, Gujarat 388421</p>
+
+						 
+			 
+			</div>
+				 </div>
+		</div>
+		 <div class="col-md-6 col-sm-6 col-xs-6" data-animate-scroll='{"x": "200","y": "0", "alpha": "0", "duration": "1.75"}'>
+		<div class="soo">
+				<div class="map-responsive so1">
+    <iframe src="https://maps.google.com/maps?q=charusat%20&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
+</div> 
+		</div>
+		</div>
+			 </div>
+			</div>
+	
+	<div class="container text-center clearFix">
+		<div class="row">
+ 
+			<div class="col-md-4 col-sm-4 col-xs-4" data-animate-scroll='{"x": "200","y": "0", "alpha": "0", "duration": "1.75"}'>
+			<img src="1.jpg" alt="" class="i img-fluid">
+			</div>
+			<div class="col-md-4 col-sm-4 col-xs-4">
+			<img src="1.jpg" alt="" class="i img-fluid">
+			</div>
+			<div class="col-md-4 col-sm-4 col-xs-4" data-animate-scroll='{"x": "-200","y": "0", "alpha": "0", "duration": "1.75"}'>
+			<img src="1.jpg" alt="" class="i img-fluid">
+			</div>
+				 
+			 </div>
+			 
+		
+		</div>
+	
+	</div>
+	 
+	
+  <script src="animate-scroll.js" type="text/javascript"></script>
+<script src="EaselPlugin.min.js" type="text/javascript"></script>
+<script src="ScrollToPlugin.min.js" type="text/javascript"></script>
+<script src="TweenMax.min.js" type="text/javascript"></script>
+													
+<script>  
+$(document).animateScroll();
+</script>
+	
+			 
+		 
+		
+</body>
+</html>
+
