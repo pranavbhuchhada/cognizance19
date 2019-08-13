00302898f28b1f3e0173a69d74bb53e848e3b6e4
chirayu-joshi <chirayujoshi1189@gmail.com>
2019-08-14 00:40:22 +0530

    right side pannel with onclick javascript functions added

--- events/index.php
+++ events/index.php
@@ -8,26 +8,60 @@
     <title>Event Page</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <style>
+        .hide { display: none; }
+        .show { display: block; }
+    </style>
 </head>
 
 <body>
+    <!-- header part will come here -->
     <header style="background-color: #22252A; height: 10vh; width: 100%;"></header>
 
-    <div class="container-fluid" style="min-height: 80vh;">
-        <div class="row">
-            <ul class="side-navbar text-center col-md-3">
-                <li><a href="./event_pages/index.php">Tech-Event 1</a></li>
-                <li><a href="./event_pages/index.php">Tech-Event 2</a></li>
-                <li><a href="./event_pages/index.php">Tech-Event 3</a></li>
-                <li><a href="./event_pages/index.php">Tech-Event 4</a></li>
-                <li><a href="./event_pages/index.php">Tech-Event 5</a></li>
-                <li><a href="./event_pages/index.php">Tech-Event 6</a></li>
-                <li><a href="./event_pages/index.php">Tech-Event 7</a></li>
-            </ul>
+    <section style="min-height: 80vh;">
+        <!-- left sided navbar -->
+        <ul class="side-navbar" role="tablist">
+            <li><p onclick="event_f1()">Tech-Event 1</p></li>
+            <li><p onclick="event_f2()">Tech-Event 2</p></li>
+            <li><p onclick="event_f3()">Tech-Event 3</p></li>
+            <li><p onclick="event_f4()">Tech-Event 4</p></li>
+            <li><p onclick="event_f5()">Tech-Event 5</p></li>
+            <li><p onclick="event_f6()">Tech-Event 6</p></li>
+            <li><p onclick="event_f7()">Tech-Event 7</p></li>
+        </ul>
+        <!-- right sided event links -->
+        <div class="event-panel">
+            <div id="section-main" class="hide show">
+                <p class="event-panel-heading hide show">EVENTS</p>
+            </div>
+            <div id="section_event_1" class="hide">
+                <p class="event-panel-heading">EVENT #1</p>
+            </div>
+            <div id="section_event_2" class="hide">
+                <p class="event-panel-heading">EVENT #2</p>
+            </div>
+            <div id="section_event_3" class="hide">
+                <p class="event-panel-heading">EVENT #3</p>
+            </div>
+            <div id="section_event_4" class="hide">
+                <p class="event-panel-heading">EVENT #4</p>
+            </div>
+            <div id="section_event_5" class="hide">
+                <p class="event-panel-heading">EVENT #5</p>
+            </div>
+            <div id="section_event_6" class="hide">
+                <p class="event-panel-heading">EVENT #6</p>
+            </div>
+            <div id="section_event_7" class="hide">
+                <p class="event-panel-heading">EVENT #7</p>
+            </div>
         </div>
-    </div>
+    </section>
 
+    <!-- footer part will come here -->
     <footer style="background-color: #1F1A24; height: 10vh; width: 100%;"></footer>
+
+    <script src="./script.js"></script>
 </body>
 
 </html>
---
+++ events/script.js
@@ -0,0 +1,85 @@
+var event = document.querySelector("#section-main");
+var event1 = document.querySelector("#section_event_1");
+var event2 = document.querySelector("#section_event_2");
+var event3 = document.querySelector("#section_event_3");
+var event4 = document.querySelector("#section_event_4");
+var event5 = document.querySelector("#section_event_5");
+var event6 = document.querySelector("#section_event_6");
+var event7 = document.querySelector("#section_event_7");
+
+function event_f1() {
+    event.classList.remove("show");
+    event2.classList.remove("show");
+    event3.classList.remove("show");
+    event4.classList.remove("show");
+    event5.classList.remove("show");
+    event6.classList.remove("show");
+    event7.classList.remove("show");
+    event1.classList.add("show");
+}
+
+function event_f2() {
+    event.classList.remove("show");
+    event1.classList.remove("show");
+    event3.classList.remove("show");
+    event4.classList.remove("show");
+    event5.classList.remove("show");
+    event6.classList.remove("show");
+    event7.classList.remove("show");
+    event2.classList.add("show");
+}
+
+function event_f3() {
+    event.classList.remove("show");
+    event1.classList.remove("show");
+    event2.classList.remove("show");
+    event4.classList.remove("show");
+    event5.classList.remove("show");
+    event6.classList.remove("show");
+    event7.classList.remove("show");
+    event3.classList.add("show");
+}
+
+function event_f4() {
+    event.classList.remove("show");
+    event1.classList.remove("show");
+    event2.classList.remove("show");
+    event3.classList.remove("show");
+    event5.classList.remove("show");
+    event6.classList.remove("show");
+    event7.classList.remove("show");
+    event4.classList.add("show");
+}
+
+function event_f5() {
+    event.classList.remove("show");
+    event1.classList.remove("show");
+    event2.classList.remove("show");
+    event3.classList.remove("show");
+    event4.classList.remove("show");
+    event6.classList.remove("show");
+    event7.classList.remove("show");
+    event5.classList.add("show");
+}
+
+function event_f6() {
+    event.classList.remove("show");
+    event1.classList.remove("show");
+    event2.classList.remove("show");
+    event3.classList.remove("show");
+    event4.classList.remove("show");
+    event5.classList.remove("show");
+    event7.classList.remove("show");
+    event6.classList.add("show");
+}
+
+function event_f7() {
+    event.classList.remove("show");
+    event1.classList.remove("show");
+    event2.classList.remove("show");
+    event3.classList.remove("show");
+    event4.classList.remove("show");
+    event5.classList.remove("show");
+    event6.classList.remove("show");
+    event7.classList.add("show");
+}
