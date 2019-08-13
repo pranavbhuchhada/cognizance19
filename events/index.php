d7108dd5defa75f47a3a01e7c5b78ab846650b30
chirayu-joshi <chirayujoshi1189@gmail.com>
2019-08-13 19:17:31 +0530

    side navigation bar in events added

--- events/index.php
+++ events/index.php
@@ -5,44 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Events</title>
+    <title>Event Page</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:700&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
-    <div style="background-color: #22252A; height: 8vh; width: 100%;"></div>
+    <header style="background-color: #22252A; height: 10vh; width: 100%;"></header>
 
-    <div class="container-fluid">
+    <div class="container-fluid" style="min-height: 80vh;">
         <div class="row">
-            <div class="event-heading col-md-4 col-6 rounded mx-auto">Tech</div>
-        </div>
-        <div class="row">
-            <div class="event-body col-md-8 col-11 rounded mx-auto pt-3">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore, ex facere doloribus unde saepe error provident odio! Soluta asperiores, voluptatem voluptate sed et vitae explicabo voluptas excepturi ratione nisi officia.</p>
-
-                <br><br>
-                <br><br>
-
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, eius aperiam! Cum nulla explicabo laboriosam distinctio, laudantium libero eaque praesentium asperiores possimus totam id culpa perferendis cupiditate molestiae, eveniet ipsam!</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="event-footer col-md-9 col-10 mx-auto">
-                <div class="container-fluid">
-                    <div class="row text-center text-white">
-                        <div class="col-lg-2 col-md-3 col-4">Description</div>
-                        <div class="col-lg-2 col-md-2 col-3">Format</div>
-                        <div class="col-lg-1 col-md-2 col-2">Rules</div>
-                        <div class="col-lg-3 col-md-2 col-3">Problem</div>
-                        <div class="col-lg-2 col-md-3 col-6">Resources</div>
-                        <div class="col-lg-2 col-md-12 col-6">Registration</div>
-                    </div>
-                </div>
-            </div>
+            <ul class="side-navbar text-center col-md-3">
+                <li><a href="./event_pages/index.php">Tech-Event 1</a></li>
+                <li><a href="./event_pages/index.php">Tech-Event 2</a></li>
+                <li><a href="./event_pages/index.php">Tech-Event 3</a></li>
+                <li><a href="./event_pages/index.php">Tech-Event 4</a></li>
+                <li><a href="./event_pages/index.php">Tech-Event 5</a></li>
+                <li><a href="./event_pages/index.php">Tech-Event 6</a></li>
+                <li><a href="./event_pages/index.php">Tech-Event 7</a></li>
+            </ul>
         </div>
     </div>
+
+    <footer style="background-color: #1F1A24; height: 10vh; width: 100%;"></footer>
 </body>
 
 </html>
